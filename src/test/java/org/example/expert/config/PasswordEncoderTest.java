c639b66b0c42ed8182feaeab739baa5b6cca353f
eashhyeon3232 <jihyeon-lou3232@naver.com>
2026-05-29 23:38:42 +0900

    refactor : SpringExtension을 MockitoExtension으로 수정
    - PasswordEncoderTest는 외부 의존성이 없는 단순 컴포넌트이므로 Mockito로 수정
    
    - Pattern을 Size(min = 8) + @Pattern으로 수정

--- src/test/java/org/example/expert/config/PasswordEncoderTest.java
+++ src/test/java/org/example/expert/config/PasswordEncoderTest.java
@@ -4,11 +4,12 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@ExtendWith(SpringExtension.class)
+@ExtendWith(MockitoExtension.class)
 class PasswordEncoderTest {
 
     @InjectMocks
